6ac678f8a69e12f2ceb347f10cf920789e8b319a
Juliana Chaves Palm <julianachavespalm@gmail.com>
2024-01-26 16:13:36 -0300

    Update UI and rename JavaScript file

---
+++ src/js/README.md
@@ -0,0 +1,62 @@
+# Aplicativo de Tradução
+
+## Objetivo
+O objetivo deste aplicativo é fornecer uma interface simples para criptografar e descriptografar texto com base em um mapa de tradução predefinido. Ele é projetado para ser uma ferramenta fácil de usar para transformar texto usando uma lógica específica de tradução.
+
+
+## Funcionalidades
+
+- **Mapa de Tradução: (translationMap)** O aplicativo possui um mapa de tradução predefinido, onde caracteres específicos são mapeados para strings de substituição correspondentes.
+
+- **Elementos da Interface do Usuário:**
+  - `inputElement`: Elemento de entrada para o texto a ser traduzido.
+  - `resultArea`: Área para exibir o texto traduzido.
+  - `resultTitle`: Área de título para exibir mensagens.
+  - `resultText`: Área de texto para exibir o texto traduzido.
+  - `copyButton`: Botão para copiar o texto traduzido para a área de transferência.
+  - `encryptButton`: Botão para iniciar o processo de criptografia.
+  - `decryptButton`: Botão para iniciar o processo de descriptografia.
+
+- **Funções:**
+  - `updateResultTitle(text)`: Atualiza a área de título com o texto fornecido.
+  - `updateResultText(text)`: Atualiza a área de texto com o texto fornecido.
+  - `updateInputText(text)`: Atualiza o elemento de entrada com o texto fornecido.
+  - `setCopyButtonVisibility(display)`: Define a visibilidade do botão de cópia com base no valor de exibição fornecido.
+  - `showEmptyInputWarning()`: Exibe um aviso quando nenhum texto de entrada é fornecido.
+  - `resetAppState()`: Reinicia o estado do aplicativo, limpando as áreas de resultado e ocultando o botão de cópia.
+  - `isInputValid()`: Valida se o texto de entrada não está vazio após a remoção de espaços em branco.
+  - `removeInputAccents`: remove os acentos: agudo (`\u0301`), circunflexo (`\u0302`), til (`\u0303`), trema (`\u030a`), círculo acima. 
+  - `copyTranslationToClipboard()`: Copia o texto traduzido para a área de transferência.
+  - `reverseTranslationMap(map)`: Inverte o mapa de tradução fornecido.
+  - `getTranslationMapForOperation(operation)`: Retorna o mapa de tradução com base na operação especificada (criptografar/descriptografar).
+  - `removeInputAccents(str)`: Remove acentos do texto de entrada para tradução.
+  - `processTextWithOperation(operation)`: Processa o texto de entrada com base na operação especificada e atualiza as áreas de resultado.
+  - `handleTranslationOperation(operation)`: Manipula a operação de tradução, exibindo avisos para entrada vazia e iniciando o processo de tradução.
+  
+- **Manipuladores de Eventos:**
+  - `encryptButton.onclick`: Inicia o processo de criptografia.
+  - `decryptButton.onclick`: Inicia o processo de descriptografia.
+
+- **Métodos Públicos:**
+  - `encrypt()`: Método público para iniciar o processo de criptografia.
+  - `decrypt()`: Método público para iniciar o processo de descriptografia.
+
+## Uso
+
+Para usar o Aplicativo de Tradução, inicialize-o usando a função `translatorApp()`. Isso retornará um objeto com os métodos `encrypt` e `decrypt` que podem ser chamados para realizar as operações correspondentes.
+
+```javascript
+// Inicialize o Aplicativo de Tradução
+const tradutor = TranslatorApp();
+
+// Exemplo: Criptografar texto
+tradutor.encrypt();
+
+// Exemplo: Descriptografar texto
+tradutor.decrypt();
+```
+
+Sinta-se à vontade para personalizar o mapa de tradução e outros aspectos do aplicativo para atender ao seu caso de uso específico.
+
+## Autora
+Juliana Chaves Palm
---
+++ src/js/translatorApp.js
@@ -0,0 +1,117 @@
+function translatorApp() {
+    const translationMap = {
+        'a': 'ai',
+        'e': 'enter',
+        'i': 'imes',
+        'o': 'ober',
+        'u': 'ufat',
+    }
+    
+    const domElements  = {
+        inputElement: document.getElementById('inputToTranslate'),
+        resultTitle: document.getElementById('resultTitle'),
+        resultText: document.getElementById('resultText'),
+        copyButton: document.getElementById('buttonToCopyTranslation'),
+        encryptButton: document.getElementById("encryptButton"),
+        decryptButton: document.getElementById("decryptButton"),
+    };
+
+    // Desestruturação de domElements
+    const { inputElement, resultTitle, resultText, copyButton, encryptButton, decryptButton } = domElements;
+    
+    // Funções de manipulação do DOM
+    function updateResult(title, text) {
+        resultTitle.textContent = title;
+        resultText.textContent = text;
+    }
+
+    function setResultVisibility(titleDisplay, textDisplay, buttonDisplay) {
+        resultTitle.style.display = titleDisplay;
+        resultText.style.display = textDisplay;
+        copyButton.style.display = buttonDisplay;
+    }
+
+    function updateInputText(text) {
+        inputElement.value = text;
+    }
+
+    // Funções de manipulação de estado
+    function showEmptyInputWarning() {
+        setResultVisibility('block', 'block', 'none');
+        updateResult('Nenhuma mensagem encontrada', 'Digite um texto que você deseja criptografar ou descriptografar');
+    }
+
+    function resetAppState() {
+        updateResult('', '');
+        setResultVisibility('none', 'none', 'none');
+    }
+
+    function isInputValid() {
+        const inputToTranslate = inputElement.value.trim();
+        return inputToTranslate.length > 0;
+    }
+
+    // Função de interação com a área de transferência
+    function copyTranslationToClipboard() {
+        const textToCopy = resultText.textContent;
+        navigator.clipboard.writeText(textToCopy)
+            .then(() => console.log(`O texto "${textToCopy}" foi copiado com sucesso!`))
+            .catch((error) => console.error('Erro ao copiar o texto:', error));
+    }
+
+    // Funções de manipulação do mapa de tradução
+    function reverseTranslationMap(map) {
+        return Object.fromEntries(Object.entries(map).map(([key, value]) => [value, key]));
+    }
+
+    function getTranslationMapForOperation(operation) {
+        return {
+            encrypt: translationMap,
+            decrypt: reverseTranslationMap(translationMap),
+        }[operation];
+    }
+
+    // Função para remover acentuação do texto de entrada para tradução
+    function removeInputAccents(text) {
+        return text.normalize("NFD").replace(/[\u0301\u0302\u0303\u0308\u030a]/g, "");
+    }
+
+    // Funções de processamento de texto
+    function processTextWithOperation(operation) {
+        const input = inputElement.value.toLowerCase();
+        const translationMap = getTranslationMapForOperation(operation);
+        const pattern = new RegExp(Object.keys(translationMap).join('|'), 'g');
+
+        const inputWithoutAccents = removeInputAccents(input);
+
+        const processedText = inputWithoutAccents.replace(pattern, match => translationMap[match]);
+
+        updateResult('', processedText);
+        updateInputText('');
+    }
+
+    // Funções de manipulação de eventos
+    function handleTranslationOperation(operation) {
+        if (!isInputValid()) {
+            showEmptyInputWarning();
+        } else {
+            resetAppState();
+            processTextWithOperation(operation);
+            setResultVisibility('none', 'block', 'block');
+        }
+    }
+
+    // Atribuição de manipuladores de eventos
+    encryptButton.onclick = () => handleTranslationOperation('encrypt');
+    decryptButton.onclick = () => handleTranslationOperation('decrypt');
+    copyButton.onclick = () => copyTranslationToClipboard();
+
+    // Métodos públicos
+    return {
+        encrypt: () => handleTranslationOperation('encrypt'),
+        decrypt: () => handleTranslationOperation('decrypt')
+    };
+}
+
+// Inicialização da aplicação
+translatorApp();
